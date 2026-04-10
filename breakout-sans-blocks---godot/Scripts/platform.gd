9e268b4f06b88a977276f6d58d3b58717f5f6f24
Domar987 <omerserifoduncu@gmail.com>
2026-04-10 23:14:47 +0300

    temel nesneler oluşturuldu

---
+++ breakout-sans-blocks---godot/Scripts/platform.gd
@@ -0,0 +1,31 @@
+extends StaticBody2D
+
+@export var y = 100.0
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+@export var color1 = Color()
+@export var color2 = Color()
+var length = 100
+var rect = Rect2()
+var rectShape = RectangleShape2D.new()
+@onready var collisionShape = $CollisionShape2D
+var mousepos:float
+
+
+func _physics_process(delta: float) -> void:
+	rectShape.size = Vector2(length, 4)
+	collisionShape.set_shape(rectShape)
+	rect = Rect2(Vector2(-length/2,-2),Vector2(length,4))
+	mousepos = get_global_mouse_position().x
+	if mousepos > get_viewport().size.x/(2*3) - length/2:
+		mousepos = get_viewport().size.x/(2*3) - length/2
+	elif mousepos < -get_viewport().size.x/(2*3) + length/2:
+		mousepos = -get_viewport().size.x/(2*3) + length/2
+	position = Vector2(mousepos,y)
+	
+func _draw():
+	#draw_rect(Rect2(Vector2(-length/2,-2),Vector2(length,4)),color)
+	draw_rect(Rect2(Vector2(-length/2+1,-1),Vector2(length-2,2)),color1,true)
+	draw_rect(Rect2(Vector2(-length/2+0.5,-1.5),Vector2(length-1,3)),color2,false,1)
+	draw_line(Vector2(length/2-3,-0.5),Vector2(length/2-1,-0.5),Color.WHITE,1.0)
+	draw_line(Vector2(length/2-1.5,0),Vector2(length/2-1.5,1.0),Color.WHITE,1.0)
