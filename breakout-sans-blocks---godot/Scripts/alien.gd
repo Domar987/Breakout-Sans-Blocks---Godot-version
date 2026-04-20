4673441e5c4fbedd324bc704b660879bfe9c4c75
Domar987 <omerserifoduncu@gmail.com>
2026-04-20 20:41:28 +0300

    uzaylı animasyonları hatalı ama renk ataması doğru çalışıyor gibi

--- breakout-sans-blocks---godot/Scripts/alien.gd
+++ breakout-sans-blocks---godot/Scripts/alien.gd
@@ -3,39 +3,41 @@ extends Area2D
 
 var alienfile = FileAccess.get_file_as_string("res://Data/alien_colors.json")
 var aliencolors = JSON.parse_string(alienfile)
+var selectedcolors:Array
 
-var aliensprites = ["res://Sprites/alien1.png","res://Sprites/alien2.png"]
+var aliensprites = ["res://Sprites/Alien/alienlight","res://Sprites/Alien/alienmain","res://Sprites/Alien/aliendark","res://Sprites/Alien/alienoutlinelight","res://Sprites/Alien/alienoutlinedark"]
 
 @onready var RuleManager = $/root/Ingame/RuleManager
 @onready var Ball = $/root/Ingame/Ball
 @onready var Wall = $/root/Ingame/Wall
-@onready var sprite:AnimatedSprite2D = $AnimatedSprite2D
+@onready var sprites:Array[AnimatedSprite2D] = [$Light,$Main,$Dark,$OutlineLight,$OutlineDark]
+@onready var deathsprite:AnimatedSprite2D = $DeathSprite
 
 var fromLorR:bool = false
 var xSpeed:float = 0.5
 var entered:bool = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var alientexture:Texture2D = load(aliensprites[randi_range(0,1)])
+	var alientexture:Array[Texture2D] = []
+	for i in range(0,5):
+		alientexture.append(load(aliensprites[i]+str(randi_range(1,2))+".png"))
 	#for x in range(alientexture.get_width()):
 		#for y in range(alientexture.get_height()):
 			#var pixel:Color = alientexture.get_image().get_pixel(x,y)
 			#if pixel == Color.WHITE:
-	var texwidth = alientexture.get_width() / 3
-	var texheight = alientexture.get_height()
-	sprite.sprite_frames.add_animation("idle")
-	for i in range(0,4):
-		var atlas = AtlasTexture.new()
-		atlas.atlas = alientexture
-		if i % 2 == 0:
-			atlas.region = Rect2(texwidth * i, 0, texwidth, texheight)
-			sprite.sprite_frames.add_frame("idle",atlas, 3.0)
-		else:
-			atlas.region = Rect2(texwidth, 0, texwidth, texheight)
-			sprite.sprite_frames.add_frame("idle",atlas, 1.0)
-	sprite.sprite_frames.set_animation_loop("idle", true)
-	sprite.sprite_frames.set_animation_speed("idle", 10.0)
-	sprite.play("idle")
+	var texwidth = alientexture[0].get_width() / 3
+	var texheight = alientexture[0].get_height()
+	for i1 in range(0,5):
+		for i in range(0,4):
+			var atlas = AtlasTexture.new()
+			atlas.atlas = alientexture[i1]
+			if i % 2 == 0:
+				atlas.region = Rect2(texwidth * i, 0, texwidth, texheight)
+				sprites[i1].sprite_frames.add_frame("idle",atlas, 3.0)
+			else:
+				atlas.region = Rect2(texwidth, 0, texwidth, texheight)
+				sprites[i1].sprite_frames.add_frame("idle",atlas, 1.0)
+		sprites[i1].play("idle")
 	
 	if randi_range(0,1):
 		fromLorR = true
@@ -43,6 +45,9 @@ func _ready() -> void:
 	var y = 4 + 32 * randi_range(-get_viewport().size.y/(32*2*RuleManager.zoom),0)
 	position = Vector2(x, y)
 	xSpeed *= -2*int(fromLorR) + 1
+	selectedcolors = selectColor(y)
+	for i in range(0,5):
+		sprites[i].modulate = Color(selectedcolors[i])
 	pass # Replace with function body.
 
 
@@ -54,15 +59,29 @@ func _process(delta: float) -> void:
 	if entered and abs(position.x) >= abs(get_viewport().size.x/(2*RuleManager.zoom)) - 2 * Wall.wallwidth:
 		xSpeed *= -1
 		position.y += 32
+		selectedcolors = selectColor(position.y)
+		for i in range(0,5):
+			sprites[i].modulate = Color(selectedcolors[i])
 	pass
 
 
 func _on_area_entered(area: Area2D) -> void:
 	if area == Ball:
 		xSpeed = 0
-		sprite.play("death")
+		for i in range(0,5):
+			sprites[i].play("death")
 
 
 func _on_animated_sprite_2d_animation_finished() -> void:
-	if sprite.animation == "death":
+	if sprites[0].animation == "death":
 		queue_free()
+
+func selectColor(y:float)->Array:
+	if y >= 32:
+		return aliencolors[3]
+	elif y >= 0:
+		return aliencolors[2]
+	elif y >= -16:
+		return aliencolors[1]
+	else:
+		return aliencolors[0]
