12365f65b1aebcffe50d6f4654013e82ebf6b09d
Domar987 <omerserifoduncu@gmail.com>
2026-04-23 17:52:20 +0300

    modüler duvar modeline geçiş

---
+++ breakout-sans-blocks---godot/Scripts/edge_border.gd
@@ -0,0 +1,25 @@
+class_name EdgeBorder extends Area2D
+
+@onready var RuleManager = $/root/Ingame/RuleManager
+var lefttexture:Texture2D
+var righttexture:Texture2D
+var wallwidth:int
+var wallheight:int
+var ySpeedAddition:float = 0.0
+var ySpeed:float = 0.0
+var oldzoom:float
+
+func _process(_delta: float) -> void:
+	ySpeedAddition -= ySpeed
+	if ySpeedAddition <= -10:
+		ySpeedAddition = 0
+	if RuleManager.zoom != oldzoom or ySpeed != 0:
+		queue_redraw()
+	oldzoom = RuleManager.zoom
+
+func _draw() -> void:
+	for i in range(0,540/(wallheight*RuleManager.zoom)+1):
+		var x = 960/(2*RuleManager.zoom) + 1
+		var y = i*wallheight-540/(2*RuleManager.zoom)
+		draw_texture(lefttexture,Vector2(-x, y + ySpeedAddition))
+		draw_texture(righttexture,Vector2(x - wallwidth, y + ySpeedAddition))
--- breakout-sans-blocks---godot/Scripts/wall.gd
+++ breakout-sans-blocks---godot/Scripts/wall.gd
@@ -1,37 +1,24 @@
-extends Area2D
+extends EdgeBorder
 
 var colShape1 = WorldBoundaryShape2D.new()
 var colShape2 = WorldBoundaryShape2D.new()
 
-var walltexleft:Texture2D = load("res://Sprites/wallleft.png")
-var walltexright:Texture2D = load("res://Sprites/wallright.png")
-const wallwidth = 12
-
-@onready var RuleManager = $/root/Ingame/RuleManager
-var oldzoom:float
-var ySpeed:float = 0.0
-var ySpeedAddition:float = 0.0
 
 func _ready() -> void:
+	lefttexture = load("res://Sprites/wallleft.png")
+	righttexture = load("res://Sprites/wallright.png")
+	wallwidth = 12
+	wallheight = 10
 	colShape1.normal = Vector2(1,0)
 	colShape2.normal = Vector2(-1,0)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	ySpeedAddition -= ySpeed
-	if ySpeedAddition <= -10:
-		ySpeedAddition = 0
 	colShape1.distance = -960/(2*RuleManager.zoom) + wallwidth
 	colShape2.distance = -960/(2*RuleManager.zoom) + wallwidth
-	if RuleManager.zoom != oldzoom or ySpeed != 0:
-		queue_redraw()
 	$CollisionShape2D1.shape = colShape1
 	$CollisionShape2D2.shape = colShape2
-	oldzoom = RuleManager.zoom
+	super(_delta)
 
 func _draw() -> void:
 	if RuleManager.walls:
-		for i in range(0,540/(5*RuleManager.zoom)+1): #kamera boyutu carpi duvar sprite'inin yuksekligi
-			var x = 960/(2*RuleManager.zoom) + 1
-			var y = i*10-540/(2*RuleManager.zoom)
-			draw_texture(walltexleft,Vector2(-x, y + ySpeedAddition))
-			draw_texture(walltexright,Vector2(x - wallwidth, y + ySpeedAddition))
+		super()
