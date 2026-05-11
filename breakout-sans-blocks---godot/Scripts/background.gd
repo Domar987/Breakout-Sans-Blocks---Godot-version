e4876c64c46a9f28b07279e49ee0eaea55a72a4e
Domar987 <omerserifoduncu@gmail.com>
2026-05-11 09:56:11 +0300

    başarsız deneme

--- breakout-sans-blocks---godot/Scripts/background.gd
+++ breakout-sans-blocks---godot/Scripts/background.gd
@@ -35,8 +35,8 @@ func _physics_process(delta: float) -> void:
 	
 	yvalue += delta * RuleManager.ySpeed
 	
-	currentcolors = bgcolors[min(int(yvalue/(4000/3)),len(bgcolors)-1)]
-	texture.gradient = getGradient(grad.gradient,yvalue/(4000)-0.1,yvalue/(4000))
+	currentcolors = bgcolors[min(int(yvalue/(4000.0/3)),len(bgcolors)-1)]
+	texture.gradient = getGradient(grad.gradient,yvalue/4000,yvalue/4000+0.11)
 	
 	$Label.text = str(yvalue)+"\n"+str(RuleManager.ySpeed)+"\n"+str(delta)
 	$Label.scale.y = (RuleManager.zoom) /960
