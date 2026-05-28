e4d09948cb404188d6912239f7da12aa6d886118
Domar987 <omerserifoduncu@gmail.com>
2026-05-28 16:45:31 +0300

    yeni düşman dizaynı

---
+++ breakout-sans-blocks---godot/Scripts/mechagator.gd
@@ -0,0 +1,28 @@
+class_name MechaGator extends Gator
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	hurtAudios = [$GatorHurt]
+	hp = 4
+	dmg = 1
+	tier = 2
+	dropChance = 20
+	shoots = true
+	sprites = [$Head,$Arm,$Body,$Fire]
+	xSpeed = 100
+	ySpeed = 0
+	projectilesource = preload("res://Objects/GatorTooth.tscn")
+	
+	super()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	launches -= 1
+	ySpeed = 0
+	super(delta)
+
+
+func ballFromBottom()->void:
+	pass
