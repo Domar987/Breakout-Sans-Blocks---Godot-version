af2990d3a57097977be8f8841970746741979c4c
Domar987 <omerserifoduncu@gmail.com>
2026-06-26 21:17:18 +0300

    arkaplan geçişi bitti duvarlar hariç o da çok zor olacak herhalde

--- breakout-sans-blocks---godot/Scripts/background.gd
+++ breakout-sans-blocks---godot/Scripts/background.gd
@@ -92,6 +92,7 @@ var rect1:Rect2
 var rect2:Rect2
 var recttrans:Rect2
 var transtexture:Texture2D = load("res://Sprites/Background/bgtransitionnew.png")
+var transheight = transtexture.get_height()
 
 var drawtrans:bool = false
 var startY:float
@@ -128,9 +129,9 @@ func _draw() -> void:
 		draw_rect(Rect2(Vector2(x,y),Vector2(-2*x,-2*y)),Color(bgcolors[level][2]))
 		firstdraw = false
 	else:
-		rect1 = Rect2(Vector2(x,y - (startY-yvalue)), Vector2(-2*x,-2*y))
-		rect2 = Rect2(Vector2(x,3 * y - (startY-yvalue)), Vector2(-2*x,-2*y))
-		recttrans = Rect2(Vector2(x,y - (startY-yvalue)), Vector2(-2*x,transtexture.get_height()))
+		rect1 = Rect2(Vector2(x,y - (startY-yvalue)), Vector2(-2*x,-2*y + transheight))
+		rect2 = Rect2(Vector2(x,3 * y - (startY-yvalue + transheight)), Vector2(-2*x,-2*y + transheight))
+		recttrans = Rect2(Vector2(x,y - (startY-yvalue)), Vector2(-2*x,transheight))
 
 		draw_rect(rect1,Color(bgcolors[level-1][2]))
 		draw_rect(rect2,Color(bgcolors[level][2]))
