87b3f984b5b818cd0abdaa331571226877e0595c
Domar987 <omerserifoduncu@gmail.com>
2026-08-11 23:50:21 +0300

    yarın devam ederim erken yatıp erken kalkmam lazım
    staj başvurusu için

--- breakout-sans-blocks---godot/Scripts/profile.gd
+++ breakout-sans-blocks---godot/Scripts/profile.gd
@@ -3,8 +3,10 @@ extends Control
 var canspin:bool = true
 var currentEnemy:int = 0
 
-var x:float
-var y:float
+var x1:float
+var y1:float
+var x2:float
+var y2:float
 var angle:float = 0.0
 
 # Called when the node enters the scene tree for the first time.
@@ -15,9 +17,12 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	#$Label.text = str(canspin)
-	x = 60 * cos(angle) - 20
-	y = 60 * sin(angle) + 40
-	get_child(0).position = Vector2(x,y)
+	x1 = 60 * cos(angle) - 20
+	y1 = 60 * sin(angle) + 40
+	x2 = 60 * cos(angle + PI) - 20
+	y2 = 60 * sin(angle + PI) + 40
+	get_child(0).position = Vector2(x1,y1)
+	get_child(1).position = Vector2(x2,y2)
 	if get_parent().visible and canspin:
 		if Input.is_action_pressed("ui_left"):
 			canspin = false
@@ -28,7 +33,7 @@ func _process(delta: float) -> void:
 
 func spin(tmp:float)->void:
 	#print("spun")
-	var tween = create_tween()
+	var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BOUNCE)
 	tween.set_parallel(false)
 	tween.tween_property(self,"angle",tmp,0.5)
 	tween.tween_callback(stupidbool)
