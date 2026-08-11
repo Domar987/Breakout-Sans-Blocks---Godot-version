e82ebe1433ebf4bb3fbb30bb1977072f2909bd38
Domar987 <omerserifoduncu@gmail.com>
2026-08-11 21:38:14 +0300

    menü devam

--- breakout-sans-blocks---godot/Scripts/profile.gd
+++ breakout-sans-blocks---godot/Scripts/profile.gd
@@ -1,5 +1,7 @@
 extends Control
 
+var canspin:bool = true
+var currentEnemy:int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +10,16 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if visible and canspin:
+		if Input.is_action_pressed("ui_left"):
+			canspin = false
+			spinLeft()
+		if Input.is_action_pressed("ui_right"):
+			canspin = false
+			spinRight()
+
+func spinLeft()->void:
+	pass
+
+func spinRight()->void:
 	pass
