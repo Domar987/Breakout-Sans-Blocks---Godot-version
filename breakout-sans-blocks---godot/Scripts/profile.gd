e7027f96ebddac84232f3f3e4f9f8c36a365a02d
Domar987 <omerserifoduncu@gmail.com>
2026-08-11 23:26:19 +0300

    salak saçma şeyler

--- breakout-sans-blocks---godot/Scripts/profile.gd
+++ breakout-sans-blocks---godot/Scripts/profile.gd
@@ -14,20 +14,23 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	$Label.text = str(canspin)
+	#$Label.text = str(canspin)
 	x = 60 * cos(angle) - 20
 	y = 60 * sin(angle) + 40
 	get_child(0).position = Vector2(x,y)
-	if get_parent().visible and Input.is_action_pressed("ui_left"):
-		spin(angle - PI)
-	if get_parent().visible and Input.is_action_pressed("ui_right"):
-		spin(angle + PI)
+	if get_parent().visible and canspin:
+		if Input.is_action_pressed("ui_left"):
+			canspin = false
+			spin(angle - PI)
+		if Input.is_action_pressed("ui_right"):
+			canspin = false
+			spin(angle + PI)
 
 func spin(tmp:float)->void:
-	if canspin:
-		canspin = false
-		print("spun")
-		var tween = create_tween()
-		tween.set_parallel(false)
-		tween.tween_property(self,"angle",tmp,0.5)
-		tween.tween_property(self,"canspin",true,0.25)
+	#print("spun")
+	var tween = create_tween()
+	tween.set_parallel(false)
+	tween.tween_property(self,"angle",tmp,0.5)
+	tween.tween_callback(stupidbool)
+func stupidbool()->void:
+	canspin = true
