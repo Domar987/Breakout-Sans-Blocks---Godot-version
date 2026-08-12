455fe7dc1342845accead14d848d3df54770645d
Domar987 <omerserifoduncu@gmail.com>
2026-08-12 16:07:43 +0300

    bütün ikonlar tek dosya olacak + alakalı kod

--- breakout-sans-blocks---godot/Scripts/profile.gd
+++ breakout-sans-blocks---godot/Scripts/profile.gd
@@ -9,6 +9,10 @@ var x2:float
 var y2:float
 var angle:float = 0.0
 
+var onSprite2:bool = false
+
+@onready var enemies:int = get_child(0).hframes
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -16,7 +20,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	#$Label.text = str(canspin)
+	$Label.text = str(currentEnemy)
 	x1 = 60 * cos(angle) - 30
 	y1 = 60 * sin(angle) + 40
 	x2 = 60 * cos(angle + PI) - 30
@@ -33,7 +37,8 @@ func _process(delta: float) -> void:
 
 func spin(sign:float)->void:
 	#print("spun")
-	currentEnemy += sign
+	enemyCounter(sign)
+	swapImage()
 	var tmp = angle + sign * PI
 	var tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BOUNCE)
 	tween.set_parallel(false)
@@ -41,3 +46,17 @@ func spin(sign:float)->void:
 	tween.tween_callback(stupidbool)
 func stupidbool()->void:
 	canspin = true
+
+func enemyCounter(amount:float)->void:
+	currentEnemy += amount
+	if currentEnemy < 0:
+		currentEnemy += enemies
+	elif currentEnemy >= enemies:
+		currentEnemy -= enemies
+
+func swapImage()->void:
+	onSprite2 = not onSprite2
+	if onSprite2:
+		get_child(1).frame = currentEnemy % enemies
+	else:
+		get_child(0).frame = currentEnemy % enemies
