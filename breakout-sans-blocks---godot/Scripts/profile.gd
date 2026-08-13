1eb8c066296e90632bd18f0365a6980066ba5aee
Domar987 <omerserifoduncu@gmail.com>
2026-08-13 21:58:38 +0300

    küçücük hatacık

--- breakout-sans-blocks---godot/Scripts/profile.gd
+++ breakout-sans-blocks---godot/Scripts/profile.gd
@@ -21,6 +21,8 @@ var onSprite2:bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	title.text = enemydescs[0][0]
+	description.text = enemydescs[0][1]
 	pass # Replace with function body.
 
 
