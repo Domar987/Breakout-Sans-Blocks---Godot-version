ea96ad7bc7e5e4b333d377207226e914f39048d0
Domar987 <omerserifoduncu@gmail.com>
2026-09-03 01:19:04 +0300

    yarın sonunda şunların bazılarını eklicem

--- breakout-sans-blocks---godot/Scripts/drop.gd
+++ breakout-sans-blocks---godot/Scripts/drop.gd
@@ -6,7 +6,7 @@ var prizeamount:int = 11
 var prizeposX:float = 0
 var prizeposY:float = 0
 var tier:int = 1
-var tiervariants = [[],[0,3],[1,2,4,6],[1,2,5,6,7,8,9],[10]]
+var tiervariants = [[],[0,3,4],[1,2,5,7],[1,2,6,7,8,9,10],[11]]
 var variant:int = 1
 var sineTimer:float = 0
 
