91990512118bb40510cd820c07e446714310c7f3
Domar987 <omerserifoduncu@gmail.com>
2026-09-08 16:25:08 +0300

    Spawner'da değişiklik, görsel değişiklikler, yeni düşman açıklamaları

--- breakout-sans-blocks---godot/Scripts/spawner.gd
+++ breakout-sans-blocks---godot/Scripts/spawner.gd
@@ -7,20 +7,21 @@ class_name Spawner extends Node
 @onready var RuleManager = $/root/Ingame/RuleManager
 @onready var background = $/root/Ingame/Background
 var numberOfEnemies:int = 0
-var timer:int
+var timer:float
 @export var spawnRare:PackedScene
 var canSpawnRare:bool = true
 @export var rareChance:int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	timer = randi_range(min(50,50 / spawnWeight), 200 / spawnWeight)
+	if spawnWeight > 0:
+		timer = randf_range(min(0.8,0.8 / spawnWeight), 3.2 / spawnWeight)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	if RuleManager.difficulty >= spawnAtDifficulty and numberOfEnemies < maxSpawned and RuleManager.health > 0:
-		timer -= 1
+		timer -= delta
 		if timer <= 0:
 			if spawnRare != null and canSpawnRare and randi_range(0,100) <= rareChance:
 				spawnEnemy(spawnRare)
@@ -28,7 +29,7 @@ func _physics_process(_delta: float) -> void:
 			else:
 				spawnEnemy(spawn)
 			if spawnWeight > 0:
-				timer = randi_range(min(50,50 / spawnWeight), 200 / spawnWeight)
+				timer = randf_range(min(0.8,0.8 / spawnWeight), 3.2 / spawnWeight)
 
 func spawnEnemy(_spawn:PackedScene)->void:
 	var newSpawn = _spawn.instantiate()
